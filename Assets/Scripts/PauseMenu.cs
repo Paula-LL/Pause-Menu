434c2a8d712c8d7c600adcd0b85495502c85587c
Paula-LL <plizelucea@gmail.com>
2025-11-06 18:49:44 +0100

    New Scripts

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup uiGroup;
     public static bool gamePaused = false;
 
     public GameObject pauseMenuUI;
@@ -15,9 +17,16 @@ public class PauseMenu : MonoBehaviour
             if (gamePaused)
             {
                 ResumeGame();
+                /*if (uiGroup.alpha < 1)
+                {
+                    uiGroup.alpha += Time.deltaTime;
+                }*/
             }
             else {
                 PauseGame();
+                /*if (uiGroup.alpha >= 1) { 
+                
+                }*/
             }
         }
     }
@@ -26,11 +35,13 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         gamePaused = false;
+        //uiGroup.alpha = 0f;
     }
 
     void PauseGame() { 
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         gamePaused = true;
+        //uiGroup.alpha = 1f;
     }
 }
